38512eb1e1e9a9e021bf5ce2b8cd71adf16a2e49
vitor.andre.ribeiro <vitor.andre.ribeiro@gmail.com>
2022-11-28 21:07:10 +0000

    login

---
+++ Academia-7621-blazer-web-app/Pages/login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Academia_7621_blazer_web_app.Pages
+{
+    public class loginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
